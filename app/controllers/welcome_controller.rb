1cadcb990428b56109e59789f2b69285d0ffdc8f
Shreya610 <48761638+Shreya610@users.noreply.github.com>
2022-12-31 12:58:55 +0530

    Add files via upload

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  def send_email
+    MyMailer.send_email(name: params[:name], phone: params[:phone], email: params[:email], message: params[:message]).deliver
+    redirect_to root_url, notice: "Email sent!"
+  end
+  def index
+  end
+end
